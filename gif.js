14bd94c483e6fcd4d2c3ee820d91909c63cb97e0
HB <hbianan@gmail.com>
2019-09-04 13:54:02 -0400

    testing
    - able to get data now

--- gif.js
+++ gif.js
@@ -1,73 +1,115 @@
+//Testing to get the data
 $(document).ready(function(){
-
-    //initial array of tv shows
     var myMovies = ["Bleach blade of faith", "hamtaro"];
-    GIFArea = "";
+    var GIFArea = "";
+    var myMovie = "";
+
+    //submit button
+    $("#add-movie").on('click', function(event) {
     
-    
-    function makeButtons() {
-        //deleting the myMovie buttons prior to adding new myMovie buttons
-        $("#myMovies-view").empty();
+        //disables auto submit
+        event.preventDefault();
         
-        for (var i=0; i < myMovies.length; i++) {
+        //gets text from the input
+        myMovie = $("#movie-input").val().trim();
         
-        //     makes buttons for my movies
-            
-            var a = $('<button>');
-
-            a.text(myMovies[i]);
-
-            a.attr('data-name', myMovies[i]);
-
-            a.addClass('myMovie');
-
-            $("#myMovies-view").append(a);
-        }
-    
-    };
-    
-    makeButtons();
-    
-//     on a button click
-    $("#add-myMovie").on('click', function() {
-    
-    //disables auto submit
-    event.preventDefault();
-    
-//     gets text from the input
-    var myMovie = $("#myMovie-input").val().trim();
-    
-//     inserts the data in the input into the array
-    myMovies.push(myMovie);
-    makeButtons();
-    
-    });
-    
-// on click
-        $(document).on('click', 'button',  function() {
-
-                $('#movieZone').empty(); 
-                var b = $(this).attr('data-name');		
-                var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + b + "&api_key=2lAj4yexlAt2PET5OG0M2Lsx29Kel7ip=12";  
+        //inserts the data in the input into the array
+        myMovies.push(myMovie);
+        console.log(myMovies)
+        // makeButtons();
+        
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + myMovie + "&api_key=2lAj4yexlAt2PET5OG0M2Lsx29Kel7ip&limit&limit=12";  
     
                 
-                $.ajax({
-                        url: queryURL,
-                        method: 'GET'
-                    })
-                    .done(function(response) {
-                        console.log(response);
-                        var results = response.data;
-                        for (var i = 0; i < results.length; i++) {
-                        var gim = $('<img>');   
-                            gim.attr('src', results[i].images.fixed_height_still.url)
-                                    .attr('data-still', results[i].images.fixed_height_still.url)
-                                    .addClass("showImage");
-                            gim.append(r)
+        $.ajax({
+            url: queryURL,
+            method: 'GET'
+        })
+        .done(function(response) {
+            console.log(response);
+            var results = response.data;
+            // for (var i = 0; i < results.length; i++) {
+            // var gim = $('<img>');   
+            //     gim.attr('src', results[i].images.fixed_height_still.url)
+            //             .attr('data-still', results[i].images.fixed_height_still.url)
+            //             .addClass("showImage");
+            //     gim.append(r)
+                                
+        });
+    });
+});
+
+// $(document).ready(function(){
+
+//     //initial array of tv shows
+//     var myMovies = ["Bleach blade of faith", "hamtaro"];
+//     GIFArea = "";
+    
+    
+//     function makeButtons() {
+//         //deleting the myMovie buttons prior to adding new myMovie buttons
+//         $("#myMovies-view").empty();
+        
+//         for (var i=0; i < myMovies.length; i++) {
+        
+//         //     makes buttons for my movies
+            
+//             var a = $('<button>');
+
+//             a.text(myMovies[i]);
+
+//             a.attr('data-name', myMovies[i]);
+
+//             a.addClass('myMovie');
+
+//             $("#myMovies-view").append(a);
+//         }
+    
+//     };
+    
+//     makeButtons();
+    
+// //     on a button click
+//     $("#add-movie").on('click', function() {
+    
+//     //disables auto submit
+//     event.preventDefault();
+    
+// //     gets text from the input
+//     var myMovie = $("#movie-input").val().trim();
+    
+// //     inserts the data in the input into the array
+//     myMovies.push(myMovie);
+//     makeButtons();
+    
+//     });
+    
+// // on click
+//         $(document).on('click', 'button',  function() {
+
+//                 $('#movieZone').empty(); 
+//                 var b = $(this).attr('data-name');		
+//                 var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + b + "&api_key=2lAj4yexlAt2PET5OG0M2Lsx29Kel7ip&limit=12";  
+    
+                
+//                 $.ajax({
+//                         url: queryURL,
+//                         method: 'GET'
+//                     })
+//                     .done(function(response) {
+//                         console.log(response);
+//                         var results = response.data;
+//                         for (var i = 0; i < results.length; i++) {
+//                         var gim = $('<img>');   
+//                             gim.attr('src', results[i].images.fixed_height_still.url)
+//                                     .attr('data-still', results[i].images.fixed_height_still.url)
+//                                     .addClass("showImage");
+//                             gim.append(r)
                               	                    
     
               
-                        }
+//                         }
     
-                    })
-                });
+//                     })
+//                 });
+// });
